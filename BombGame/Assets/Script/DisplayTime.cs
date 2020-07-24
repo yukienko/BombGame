6a07cdcce526c2b6f433effe685f8c26e3bdf680
Yazumatto <yazumatto73@gmail.com>
2020-07-24 09:28:58 +0900

    ポーズパネルの作成した

---
+++ BombGame/Assets/Script/DisplayTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTime : MonoBehaviour
+{
+    Text timeText = default;
+    private float time = 0f;
+
+    private void Start()
+    {
+        timeText = gameObject.GetComponent<Text>();
+    }
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timeText.text = time.ToString("f3");
+    }
+}
---
+++ BombGame/Assets/Script/Panel/PausePanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePanel : DisplayPanel
+{
+    protected override void Init()
+    {
+        base.Init();
+        Time.timeScale = 1f;
+    }
+
+    protected override void Hide()
+    {
+        base.Hide();
+        Time.timeScale = 1f;
+    }
+
+    protected override void Show()
+    {
+        base.Show();
+        Time.timeScale = 0f;
+    }
+}
