bb6326a38cf6bc0ebeb6df2dedf192049b91aa63
Yazumatto <yazumatto73@gmail.com>
2020-07-27 23:37:49 +0900

    敵の作成

---
+++ BombGame/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public ENEMYCOLOR enemyColor;
+    public enum ENEMYCOLOR
+    {
+        red,
+        blue,
+        yellow,
+        green
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        //初期化
+    }
+}
