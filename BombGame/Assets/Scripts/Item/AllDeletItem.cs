59fb6c32de7abdc96c3ed4944f110128bbafcee3
Yazumatto <yazumatto73@gmail.com>
2020-07-28 00:12:20 +0900

    アイテムの作成（中身はまだ）

---
+++ BombGame/Assets/Scripts/Item/AllDeletItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllDeletItem : ItemBase
+{
+    public override void UseItem()
+    {
+        base.UseItem();
+        AllDelete();
+    }
+
+    private void AllDelete()
+    {
+        Debug.Log("デレて");
+        Finish();
+    }
+}
---
+++ BombGame/Assets/Scripts/Item/DecreaseEnemyLimitItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecreaseEnemyLimitItem : ItemBase
+{
+    public override void UseItem()
+    {
+        base.UseItem();
+        DecreaseEnemyLimit();
+    }
+
+    private void DecreaseEnemyLimit()
+    {
+        Debug.Log("これだけパッシブなんでややこしいよ");
+        Finish();
+    }
+}
