6bbed50e018ee5bd4f9c0223c3ebd51b4401fe51
Yazumatto <yazumatto73@gmail.com>
2020-07-31 11:55:51 +0900

    シングルトンを作成

---
+++ BombGame/Assets/Scripts/Singleton/SingletonMonoBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);
+                if (instance== null)
+                {
+                    GameObject singleton = new GameObject();
+                    singleton.name = t.ToString() + "(singleton)";
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+}
