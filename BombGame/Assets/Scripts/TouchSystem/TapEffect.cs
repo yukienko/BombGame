55146f606a16ded9c5c788266d89a7c9f94d3142
pf_yukineko <yukineko1442@gmail.com>
2020-08-04 00:49:35 +0900

    スマホ向けの修正（色々おかしかった）。テストapkの出力

--- BombGame/Assets/Scripts/TouchSystem/TapEffect.cs
+++ BombGame/Assets/Scripts/TouchSystem/TapEffect.cs
@@ -38,13 +38,10 @@ public class TapEffect : MonoBehaviour
             {
                 // 座標系変換
                 Vector3 pos = default;
-                Vector3 world_point = Camera.main.ScreenToWorldPoint(_touchManager._touch_position);
                 if (old_phase._touch_phase == TouchPhase.Began)
                 {
                     // マウスのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
                     pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
-                    //tapEffect.transform.position = pos;
-                    //tapEffect.Emit(1);
                 }
                 else if (_touchManager._touch_phase == TouchPhase.Moved)
                 {
@@ -89,19 +86,15 @@ public class TapEffect : MonoBehaviour
             else
             {
                 // 座標系変換
-                Vector3 world_point = Camera.main.ScreenToWorldPoint(_touchManager._touch_position);
                 Vector3 vPoint = _touchManager._touch_position;
 
-                Vector3 pos;
+                Vector3 pos = default;
 
                 //クリックした瞬間
                 if (_touchManager._touch_phase == TouchPhase.Began)
                 {
                     // タップのワールド座標までパーティクルを移動し、パーティクルエフェクトを1つ生成する
                     pos = _camera.ScreenToWorldPoint(vPoint + _camera.transform.forward * 10);
-                    //tapEffect.transform.position = pos;
-                    //tapEffect.Emit(1);
-
                 }
                 //クリック長押し中（クリックした瞬間の次フレームから離すまでの間）
                 else if(_touchManager._touch_phase == TouchPhase.Moved)
@@ -119,20 +112,25 @@ public class TapEffect : MonoBehaviour
                     if (isMove)
                     {
                         pos = _camera.ScreenToWorldPoint(vPoint + _camera.transform.forward * 10);
-                        if (pos.x < gameBottomLeft || pos.x > gameTopRight || pos.y > gameTop)
-                            return;
+                        if (vPoint.x < gameBottomLeft || vPoint.x > gameTopRight)
+                        {
+                            pos = new Vector3(oldPos.x, pos.y, pos.z);
+                        }
+                        if (vPoint.y > gameTop || vPoint.y < gameButtom)
+                        {
+                            pos = new Vector3(pos.x, oldPos.y, pos.z);
+                        }
                         hitobject.transform.position = pos;
                     }
-                    else if (_touchManager._touch_phase == TouchPhase.Ended)
-                    {
-                        isMove = false;
-                    }
                 }
+                else if (_touchManager._touch_phase == TouchPhase.Ended)
+                {
+                    isMove = false;
+                }
+                oldPos = pos;
             }
         }
         //oldの更新
         old_phase._touch_phase = _touchManager._touch_phase;
-
-        Debug.Log(_touchManager._touch_position);
     }
 }
