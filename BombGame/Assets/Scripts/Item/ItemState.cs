b17bbd14f38ccdc6bce67c89abf0bc2d0a068f39
Yazumatto <yazumatto73@gmail.com>
2020-08-17 00:32:05 +0900

    アイテムの作成（2/4）
    ・敵の停止
    ・敵の全削除

---
+++ BombGame/Assets/Scripts/Item/ItemState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemState
+{
+    public enum ITEMSTATE
+    {
+        canUse,
+        unUsed,
+        isUse,
+        finish,
+    }
+
+    public static ITEMSTATE StopTimeItemState;
+    public static ITEMSTATE AllDeleteItemState;
+    public static ITEMSTATE BarrierItemState;
+    public static ITEMSTATE DecreaseEnemyLimitItemState;
+}
