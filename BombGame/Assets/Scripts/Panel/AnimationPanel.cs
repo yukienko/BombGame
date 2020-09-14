10874b0c4e6eb0c89ce688f9a9351168575862ce
Yazumatto <yazumatto73@gmail.com>
2020-09-14 20:04:36 +0900

    パネル表示のスクリプトを作成
    途中

---
+++ BombGame/Assets/Scripts/Panel/AnimationPanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPanel : DisplayPanel
+{
+    float animationTime = 0;
+    protected RectTransform animationPanelTransform = default;
+
+    [SerializeField] private Transform canvas = default;
+    [SerializeField] private Transform staticObjecctCanvas = default;
+
+    protected override void Init()
+    {
+        SetAnimationTransform();
+        base.Init();
+    }
+
+    protected virtual void SetAnimationTransform()
+    {
+        animationPanelTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    protected override void ShowPanel()
+    {
+        transform.SetParent(canvas);
+        animationPanelTransform.
+    }
+
+    protected override void HidePanel()
+    {
+        base.HidePanel();
+    }
+
+    protected virtual void OnCompleteHideAnimation()
+    {
+        transform.SetParent(staticObjecctCanvas);
+    }
+}
