3d8696c07caee26411c12d8fcc63ed7808bffe1e
Yazumatto <yazumatto73@gmail.com>
2021-02-06 17:43:02 +0900

    パネルの表示を制御するクラスを作成

--- BombGame/Assets/Scripts/Panel/ResultPanel.cs
+++ BombGame/Assets/Scripts/Panel/ResultPanel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectColorGenerateItem : ActiveItemBase<SelectColorGenerateItem>
+public class ResultPanel : DisplayPanel
 {
 
 }
